71bb012f6abd204d453a0672a6b98535011b9ae6
Ylenia <ylenia.d.23@gmail.com>
2020-05-02 19:35:28 +0000

    routing express server

--- first_express_server/src/index.js
+++ first_express_server/src/index.js
@@ -8,7 +8,7 @@ app.disable('x-powered-by');
 
 app.set('env', 'development');
 
-app.use(logger('combined'));
+//app.use(logger('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -17,10 +17,25 @@ app.set('view engine', 'pug');
 
 app.get('/', (req, res)=>{
     //res.send('<h1>Hola mundo</h1>');
-    res.render('home', {
+    /*res.render('home', {
         title: 'First layout',
         message: 'Hello World'
-    });
+    });*/
+    res.write(`
+        <h1>Curso Nodejs</h1>
+        <a href="/temario">Temario</a>
+    `)
+
+    res.end();
+});
+
+app.get('/temario', (req, res)=>{
+    res.write(`
+        <h1>Temario</h1>
+        <a href="/">Volver</a>
+    `)
+
+    res.end();
 });
 
 app.listen('9000', ()=>{
