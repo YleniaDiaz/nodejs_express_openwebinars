3a8a8204e7456c46b8b651b8ffce504b1685a948
Ylenia <ylenia.d.23@gmail.com>
2020-05-02 18:39:58 +0000

    configuring express_server

--- first_express_server/src/index.js
+++ first_express_server/src/index.js
@@ -1,6 +1,15 @@
 import express from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
 
 const app = express();
+app.disable('x-powered-by');
+
+app.set('env', 'development');
+
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', (req, res)=>{
     res.send('<h1>Hola mundo</h1>');
