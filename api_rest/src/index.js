1be01ed7fea1fcd2c07a42bcbd6b0e9a4afe95e3
Ylenia <ylenia.d.23@gmail.com>
2020-05-19 12:16:19 +0000

    routing

--- api_rest/src/index.js
+++ api_rest/src/index.js
@@ -10,6 +10,23 @@ const server ={
 
         config(app);
 
+        //ROUTES
+        app.get('/', (req, res, next)=>{
+            res.status(200).json({data:'get method'});
+        });
+
+        app.post('/resourcer', (req, res, next)=>{
+            res.status(201).json({data:'post method'});
+        });
+
+        app.put('/', (req, res, next)=>{
+            res.status(201).json({data:'put method'});
+        });
+
+        app.delete('/', (req, res, next)=>{
+            res.status(200).json({data:'deleted method'});
+        });
+
         _server=app.listen('9000', ()=>{
             if(process.env.NODE_ENV !== 'test'){
                 console.log('SERVER OK http://localhost:9000');
